e7efe027bfd33b415b09fff1be085805a1fc0be9
Yasmin123-design <https://github.com/Yasmin123-design/SocialMediaApp_Frontend.git>
2026-01-03 01:43:00 +0200

    add test controller

---
+++ E-PharmaHub/Controllers/TestController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace E_PharmaHub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public TestController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("testdb")]
+        public async Task<IActionResult> TestDb()
+        {
+            try
+            {
+                using var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+                await connection.OpenAsync();
+                return Ok("DB connection successful!");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
