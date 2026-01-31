3ddf16b5b89e9d442786fd80a771911531a45e40
Yasmin123-design <https://github.com/Yasmin123-design/SocialMediaApp_Frontend.git>
2026-01-31 04:27:04 +0200

    remove set cookie from reset password

--- E-PharmaHub/Controllers/UserController.cs
+++ E-PharmaHub/Controllers/UserController.cs
@@ -238,26 +238,7 @@ namespace E_PharmaHub.Controllers
             var result = await _userManager.ResetPasswordAsync(user, model.Token, model.NewPassword);
             if (!result.Succeeded) return BadRequest(result.Errors);
 
-            // Automatically log the user in after password reset
-            var roles = await _userManager.GetRolesAsync(user);
-            var token = GenerateJwtToken(user, roles);
-            var refreshToken = GenerateRefreshToken();
-            await _userService.SaveRefreshTokenAsync(user.Id, refreshToken);
-
-            SetAuthCookies(token, refreshToken, roles.FirstOrDefault() ?? "User");
-
-            var userObj = new
-            {
-                user.UserName,
-                user.Email,
-                Roles = roles
-            };
-
-            return Ok(new 
-            { 
-                message = "Password has been reset successfully and you are now logged in!",
-                user = userObj
-            });
+            return Ok(new { message = "Password has been reset successfully! Please log in with your new password." });
         }
         private string GenerateJwtToken(AppUser user, IList<string> roles)
         {
